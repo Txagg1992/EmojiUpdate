e4f17467917e89739b1080b29cc3ce6f5922461f
Enoch Phetteplace <curiousaps@gmail.com>
2025-05-07 11:40:40 -0500

    init commit

---
+++ app/src/main/java/com/curiousapps/emojiapiupdate/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.curiousapps.emojiapiupdate.util
+
+import kotlinx.coroutines.Dispatchers
+
+const val BASE_URL = ""
+const val URL_EXT = ""
+const val URL_ONE = ""
+
+val IO_DISPATCHER = Dispatchers.IO
